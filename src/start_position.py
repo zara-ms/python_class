dbfabb4d57f64841f27dd61e701e6bc80183924b
zarams <zaram042001@gmail.com>
2021-04-14 19:22:14 -0500

    Se agregó el archivo de la tarea start_position.py

---
+++ src/start_position.py
@@ -0,0 +1,51 @@
+"""
+## NAME
+    start_position.py
+
+
+## VERSION
+
+    [0.0]
+
+## AUTHOR
+
+    Zara Paulina Martinez Sanchez <zaram042001@gmail.com
+
+## DATE
+
+    14/04/2021
+
+## DESCRIPTION
+
+    Dada una secuencia de DNA el programa imprime la posición del codón de inicio AUG y la última posición del
+    codón de paro (UAA), así como la secuencia que se transcribe dada en DNA.
+
+## CATEGORY
+
+    Sequence analysis
+
+## USAGE
+
+    start_position.py no cuenta con más opciones
+
+## ARGUMENTS
+
+    No hay argumentos.
+
+## EXAMPLES
+    Input:
+        Secuencia de DNA a analizar "AAGGTACGTCGCGCGTTATTAGCCTAAT"
+    Output:
+        El codón de inicio empieza en la posición 4
+        El codón de paro termina en la posición 26
+        El fragmento que se transcribe (en DNA) es: TACGTCGCGCGTTATTAGCC
+
+"""
+
+dna_sec = "AAGGTACGTCGCGCGTTATTAGCCTAAT"
+inicio = dna_sec.find("TAC")
+final = dna_sec.find("TAA")
+transcrito = dna_sec[inicio:final]
+print("El codón de inicio empieza en la posición " + str(inicio))
+print("El codón de paro termina en la posición " + str(final + 2))
+print("El fragmento que se transcribe (en DNA) es: " + str(transcrito))
