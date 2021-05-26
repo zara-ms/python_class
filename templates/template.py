92774f907eeb7dd4de315a83e4163d64001d2aed
zarams <zaram042001@gmail.com>
2021-05-26 16:22:39 -0500

    Se actualizó el template

--- templates/template.py
+++ templates/template.py
@@ -1,30 +1,60 @@
 ´´´
 ## NAME
+	[programName].py
 
 
 ## VERSION
 
+	[#.#]
 
 ## AUTHOR
 
-    Zara Paulina Martinez Sanchez
+	Zara Paulina Martinez Sanchez <zaram042001@gmail.com>
+	[Other authors]: [Modifications]
+
 
 ## DATE
 
+	[dd/mm/yyyy]
 
 ## DESCRIPTION
 
+	 [briefly describe what the program does]
 
 ## CATEGORY
 
+	[category of the program: sequence analysis for example]
 
 ## USAGE
 
+	[programName][-options/arguments]
 
 ## ARGUMENTS
 
+	[name]  [description]
+        [name]  [description]
 
-## SEE ALSO
+	parser.add_argument("-i", "--input",
+                    metavar="path/to/file",
+                    help="Input file",
+                    required=True)
+	parser.add_argument("-o", "--output",
+                    help="Output file",
+                    required=False)
+
+## FUNCTIONS
+
+	funtionName(arguments):
+    	[Description]
+    	return()
+
+## EXAMPLES
+
+	[Example 1: describe the example, input and outputs]
+
+## GITHUB LINK
+
+	[Link]
 
 
 ´´´
